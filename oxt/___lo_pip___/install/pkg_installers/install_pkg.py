eebab209626d75344f2e03218922c898cef0bf94
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2023-12-03 21:33:10 -0500

    fix for uninstall

--- oxt/___lo_pip___/install/pkg_installers/install_pkg.py
+++ oxt/___lo_pip___/install/pkg_installers/install_pkg.py
@@ -82,6 +82,33 @@ class InstallPkg:
             cmd.append(f"--log={log_file}")
         return cmd
 
+    def _uninstall_pkg(self, pkg: str) -> bool:
+        # pip uninstall -y package1 package2 package3
+        cmd = ["uninstall", "-y"]
+        cmd = self._cmd_pip(*[*cmd, pkg])
+        self._logger.debug(f"Running command {cmd}")
+        self._logger.info(f"Uninstalling package {pkg}")
+        msg = f"Pip Uninstall success for: {pkg}"
+        err_msg = f"Pip Uninstall failed for: {pkg}"
+        if STARTUP_INFO:
+            process = subprocess.run(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=self._get_env(), startupinfo=STARTUP_INFO
+            )
+        else:
+            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=self._get_env())
+        result = False
+        if process.returncode == 0:
+            self._logger.info(msg)
+            result = True
+        else:
+            self._logger.error(err_msg)
+            try:
+                self._logger.error(process.stderr.decode("utf-8"))
+            except Exception as err:
+                self._logger.error(f"Error decoding stderr: {err}")
+
+        return result
+
     def _install_pkg(self, pkg: str, ver: str, force: bool) -> bool:
         """
         Install a package.
@@ -207,6 +234,10 @@ class InstallPkg:
                 break
 
             ver_lst: List[str] = [rule.get_versions_str() for rule in rules]
+            pkg_ver = self.get_package_version(name)
+            if pkg_ver:
+                if not self._uninstall_pkg(name):
+                    return False
             result = result and self._install_pkg(name, ",".join(ver_lst), force)
         self._logger.info("Installing packages Done!")
         return result
